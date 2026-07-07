c9061fbb28e8e25f839bd6dccdae3c051e6fcb7c
Bogdan Kolesnyk <bogdan.kolesnyk@gmail.com>
2026-07-07 23:24:02 +0200

    fix: failing install

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,7 @@
 import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 
 const directory = import.meta.dirname;
 // eslint-disable-next-line n/no-process-exit
 if (!directory.includes('node_modules')) process.exit(0);
-await import(path.join(directory, '../dist/postinstall.js'));
+await import(pathToFileURL(path.join(directory, '../dist/postinstall.js')));
